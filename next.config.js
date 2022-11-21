46532dc11f61e00fb391a730952b72352fa96d98
AzizulHoque <azizulh8774@gmail.com>
2022-11-21 22:41:39 +0600

    backend url .env to next.cofig.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: "/beatnikfront",
   assetPrefix: "/beatnikfront",
+  BACKEND_URL: 'http://127.0.0.1:8000'
+  
 };
 
 module.exports = nextConfig
